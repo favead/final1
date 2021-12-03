0405b56ea34b73e387779c408c39e26058bcd4bc
favead <diablo3_monk@mail.ru>
2021-12-04 01:58:27 +0300

    st

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+import './application.css'
 
 Rails.start()
 Turbolinks.start()
