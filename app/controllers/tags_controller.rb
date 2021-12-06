145c92ad7d587f5603c52f502c911bd3157e7401
favead <diablo3_monk@mail.ru>
2021-12-06 16:42:05 +0300

    f

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,7 +4,7 @@ class TagsController < ApplicationController
 		@post_tags = PostTag.where(:tag_id => params[:id])
 		@tag = Tag.find(params[:id]).title
 		@post_ids = @post_tags.map(&:post_id)
-		@posts = Post.where(:id => @post_ids)
+		@posts = Post.where(:id => @post_ids,:published => true)
 
 	end
 
